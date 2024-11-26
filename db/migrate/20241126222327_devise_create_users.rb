c568b96e8c6a49ab6001e8fd350d3d9a77f75c5d
misuzuebihara <ebiharamisuzu@gmail.com>
2024-11-26 14:40:18 -0800

    users DB

--- db/migrate/20241126222327_devise_create_users.rb
+++ db/migrate/20241126222327_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false;
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name_last,            null: false;
+      t.string :name_first,           null: false;
+      t.string :name_last_kana,       null: false;
+      t.string :name_first_kana,      null: false;
+      t.date :birthday,                null: false;
 
       ## Recoverable
       t.string   :reset_password_token
