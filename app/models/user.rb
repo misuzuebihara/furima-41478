839ba530df1cc16b9843b10839301227f0222c3b
misuzuebihara <ebiharamisuzu@gmail.com>
2024-11-26 18:25:36 -0800

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@/, message: 'must contain @' }
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must contain both letters and numbers' }
+  validates :password_confirmation, presence: true, comparison: { equal_to: :password }
+  validates :name_last, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :name_first, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :name_last_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+  validates :name_first_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday, presence: true
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
