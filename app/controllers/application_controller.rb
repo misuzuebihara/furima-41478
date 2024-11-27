63d2a3c7b3e9fb57ae71f42b137e4392a473c0cf
misuzuebihara <ebiharamisuzu@gmail.com>
2024-11-26 23:06:42 -0800

    devise_parameter_sanitizerメソッド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :users_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 
   private
 
