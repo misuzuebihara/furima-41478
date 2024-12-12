8b0acb72af9df4cc5792d16c3722cd29f7459ab8
misuzuebihara <ebiharamisuzu@gmail.com>
2024-12-11 21:30:58 -0800

    active hash

--- Gemfile
+++ Gemfile
@@ -82,3 +82,5 @@ group :production do
 end
 
 gem 'devise'
+
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-  belongs_to :user
-  has_one :purchase
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
+  validates :condition_id, numericality: { other_than: 1, message: 'を入力してください' }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: 'を入力してください' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を入力してください' }
+  validates :shipping_date_id, numericality: { other_than: 1, message: 'を入力してください' }
 end
